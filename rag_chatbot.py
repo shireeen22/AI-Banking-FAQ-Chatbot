8d27aa0a9682a684bc91c48cfa0f77c8a5a0a3a0
Shireen <shireeenk2002@gmail.com>
2026-07-02 12:05:08 +0530

    Update rag_chatbot.py

--- rag_chatbot.py
+++ rag_chatbot.py
@@ -2,10 +2,12 @@ from groq import Groq
 from dotenv import load_dotenv
 from faiss_search import get_answer
 import os
-
+import streamlit as st
 load_dotenv()
 
-chatbot = Groq(api_key=os.getenv("GROQ_API_KEY"))
+api_key = st.secrets.get("GROQ_API_KEY", os.getenv("GROQ_API_KEY"))
+
+chatbot = Groq(api_key=api_key)
 
 def rag_answer(user_query):
     retrival_result = get_answer(user_query)
